77b763a90e4e11db69498d113172bb527adcabee
himasha vithanage <himadilvithanage@gmail.com>
2020-05-05 23:47:51 +0530

    api chaned

--- DoctorServiceN/src/com/DoctorsAPI.java
+++ DoctorServiceN/src/com/DoctorsAPI.java
@@ -31,18 +31,14 @@ public class DoctorsAPI extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//NOT USED
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
